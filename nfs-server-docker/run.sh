21008a254d7eb020104e8b0037258ac5e6a4375f
guoning <guoning@clustertech.com>
2020-08-25 17:46:59 +0800

    feat:nfs

--- nfs-server-docker/run.sh
+++ nfs-server-docker/run.sh
@@ -1,2 +1,2 @@
 docker build -t cpms:nfs-server .
-docker run -d --name nfs-server -p 6666:6666 -it cpms:nfs-server --privileged=true /usr/sbin/init
+docker run -d --name nfs-server -p 6666:6666 -it cpms:nfs-server /usr/sbin/init
